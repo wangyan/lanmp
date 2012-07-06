7a820b04f8869bd5cba00ff5d2d3dd98c7b3101a
WangYan <WangYan@188.com>
2012-07-07 01:31:05 +0800

    修复无法获取最新版本MySQL的BUG

--- rpm.sh
+++ rpm.sh
@@ -217,7 +217,7 @@ groupadd mysql
 useradd -g mysql -M -s /bin/false mysql
 
 if [ ! -s mysql-*.tar.gz ]; then
-	LATEST_MYSQL_LINK=`elinks ftp://mirror.csclub.uwaterloo.ca/mysql/Downloads/MySQL-5.5/ | awk '/ftp:.+\.[0-9][0-9]\.tar\.gz$/{print $2}' | tail -n 1`
+	LATEST_MYSQL_LINK=`elinks ftp://mirror.csclub.uwaterloo.ca/mysql/Downloads/MySQL-5.5/ | awk '/ftp:.+\.[0-9][0-9][a-z]?\.tar\.gz$/{print $2}' | tail -n 1`
 	BACKUP_MYSQL_LINK='http://wangyan.org/download/lanmp/mysql-latest.tar.gz'
 	Extract ${LATEST_MYSQL_LINK} ${BACKUP_MYSQL_LINK}
 else
