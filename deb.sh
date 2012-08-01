647399b05d51f03ffc73cb408b0d18ac26fb609c
WangYan <WangYan@188.com>
2012-08-01 19:05:22 +0800

    安装结束后移动源码到src目录保存

--- deb.sh
+++ deb.sh
@@ -988,6 +988,11 @@ rm -rf /usr/local/mysql/data/test/
 rm update_mysql.sh
 rm /tmp/create_tables.sql
 
+if [ -d "$LANMP_PATH/src" ];then
+	mkdir -p $LANMP_PATH/src/
+fi
+mv $LANMP_PATH/{*gz,*-*/,ioncube,package.xml} $LANMP_PATH/src
+
 clear
 echo ""
 echo "===================== Install completed ====================="
