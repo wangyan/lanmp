d09a8e55398bf7e9adc703d01bcaf09a458e6f6b
WangYan <WangYan@188.com>
2012-08-05 15:59:50 +0800

    完善卸载脚本

--- uninstall.sh
+++ uninstall.sh
@@ -11,7 +11,7 @@ clear
 echo "#############################################################"
 echo "# LANMP Auto Uninstall Shell Scritp"
 echo "# Env: Debian/Ubuntu/Redhat/CentOS"
-echo "# Last modified: 2012.02.20"
+echo "# Last modified: 2012.08.05"
 echo ""
 echo "# Copyright (c) 2011, WangYan <WangYan@188.com>"
 echo "# All rights reserved."
@@ -33,111 +33,139 @@ fi
 
 echo "Are you sure uninstall LANMP? (y/n)"
 read -p "(Default: n):" UNINSTALL
-if [ "$UNINSTALL" = "" ]; then
+if [ -z $UNINSTALL ]; then
 	UNINSTALL="n"
 fi
+if [ "$UNINSTALL" != "y" ]; then
+	clear
+	echo "==========================="
+	echo "You canceled the uninstall!"
+	echo "==========================="
+	exit
+else
+	echo "---------------------------"
+	echo "Yes, I decided to uninstall!"
+	echo "---------------------------"
+	echo ""
+fi
+
+get_char()
+{
+SAVEDSTTY=`stty -g`
+stty -echo
+stty cbreak
+dd if=/dev/tty bs=1 count=1 2> /dev/null
+stty -raw
+stty echo
+stty $SAVEDSTTY
+}
+echo "Press any key to start uninstall..."
+echo "Or Ctrl+C cancel and exit ?"
+char=`get_char`
 echo ""
 
 if [ "$UNINSTALL" = 'y' ]; then
 
-	get_char()
-	{
-	SAVEDSTTY=`stty -g`
-	stty -echo
-	stty cbreak
-	dd if=/dev/tty bs=1 count=1 2> /dev/null
-	stty -raw
-	stty echo
-	stty $SAVEDSTTY
-	}
-	echo ""
-	echo "Press any key to start uninstall..."
-	echo "Or Ctrl+C cancel and exit ?"
-	char=`get_char`
+	echo "---------- MySQL ----------"
 
-echo "---------- mysql ----------"
+	if cat /proc/version | grep -Eqi '(redhat|centos)';then
+		chkconfig mysql off
+	elif cat /proc/version | grep -Eqi '(debian|ubuntu)';then
+		update-rc.d -f mysql remove
+	fi
 
 	/etc/init.d/mysql stop
 	killall mysqld
 	userdel mysql
 	groupdel mysql
-	update-rc.d -f mysql remove
-	chkconfig mysql off
 	rm -rf /etc/init.d/mysql
 	rm -rf /usr/local/mysql
-	rm -rf /etc/mysql
+	rm -rf /etc/my.cnf
 	rm -rf /usr/bin/mysql*
 	sed -i 's/\/usr\/local\/mysql\/lib//g' /etc/ld.so.conf
 
-echo "---------- php5 ----------"
+	echo "---------- Apache ----------"
 
-	cd $LANMP_PATH/libpng-*/
-	make uninstall
-
-	cd $LANMP_PATH/jpeg-*/
-	make uninstall
-
-	cd $LANMP_PATH/libiconv-*/
-	make uninstall
-
-	cd $LANMP_PATH/libmcrypt-*/
-	make uninstall
-
-	cd $LANMP_PATH/mhash-*/
-	make uninstall
-
-	cd $LANMP_PATH/mcrypt-*/
-	make uninstall
-
-	/usr/local/php/sbin/php-fpm stop
-	killall php-fpm
-	update-rc.d -f php-fpm remove
-	chkconfig php-fpm off
-	rm -rf /etc/init.d/php-fpm
-	rm -rf /usr/local/php
-	rm -rf /usr/local/zend
-	rm -rf /tmp/eaccelerator
-	rm -rf /usr/bin/php*
-
-echo "---------- nginx ----------"
-
-	cd $LANMP_PATH/pcre-*/
-	make uninstall
-
-	/etc/init.d/nginx stop
-	killall nginx
-	userdel nginx
-	groupdel nginx
-	update-rc.d -f nginx remove
-	chkconfig 345 nginx off
-
-	rm -rf /etc/init.d/nginx
-	rm -rf /usr/local/nginx
-	rm -rf /usr/sbin/nginx*
-
-echo "---------- apache ----------"
+	if cat /proc/version | grep -Eqi '(redhat|centos)';then
+		chkconfig httpd off
+	elif cat /proc/version | grep -Eqi '(debian|ubuntu)';then
+		update-rc.d -f httpd remove
+	fi
 
 	/etc/init.d/httpd stop
 	killall httpd
 	userdel www
 	groupdel www
-	update-rc.d -f httpd remove
-	chkconfig httpd off
 	rm -rf /etc/init.d/httpd
 	rm -rf /usr/local/apache
+	rm -rf /usr/local/apr
 	rm -rf /usr/bin/apachectl
 	rm -rf /usr/bin/httpd
 	rm -rf /usr/bin/ab
 
-echo "---------- phpmyadmin ----------"
+	echo "---------- PHP ----------"
+
+	cd $LANMP_PATH/src/libpng-*/
+	make uninstall
+
+	cd $LANMP_PATH/src/jpeg-*/
+	make uninstall
+
+	cd $LANMP_PATH/src/libiconv-*/
+	make uninstall
+
+	cd $LANMP_PATH/src/libmcrypt-*/
+	make uninstall
+
+	cd $LANMP_PATH/src/mhash-*/
+	make uninstall
+
+	cd $LANMP_PATH/src/mcrypt-*/
+	make uninstall
+
+	if [ -s /usr/local/php/sbin/php-fpm ]; then
+		/usr/local/php/sbin/php-fpm stop
+		killall php-fpm
+		if cat /proc/version | grep -Eqi '(redhat|centos)';then
+			chkconfig php-fpm off
+		elif cat /proc/version | grep -Eqi '(debian|ubuntu)';then
+			update-rc.d -f php-fpm remove
+		fi
+		rm -rf /etc/init.d/php-fpm
+	fi
+
+	rm -rf /usr/local/php
+	rm -rf /usr/local/zend
+	rm -rf /var/www/xcache
+	rm -rf /tmp/{pcov,phpcore}
+	rm -rf /usr/bin/php*
+
+	echo "---------- nginx ----------"
+
+	cd $LANMP_PATH/src/pcre-*/
+	make uninstall
+
+	if cat /proc/version | grep -Eqi '(redhat|centos)';then
+		chkconfig nginx off
+	elif cat /proc/version | grep -Eqi '(debian|ubuntu)';then
+		update-rc.d -f nginx remove
+	fi
+
+	/etc/init.d/nginx stop
+	killall nginx
+	userdel nginx
+	groupdel nginx
+
+	rm -rf /etc/init.d/nginx
+	rm -rf /usr/local/nginx
+	rm -rf /var/tmp/nginx
+	rm -rf /usr/sbin/nginx*
+
+	echo "---------- phpmyadmin ----------"
 
 	rm -rf /var/www/phpmyadmin
 
 	echo "==========================="
 	echo "Uninstall completed!"
-	echo "==========================="
-else
-	echo "==========================="
-	echo "You canceled uninstall!"
 	echo "==========================="
 fi
