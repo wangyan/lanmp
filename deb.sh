ce9bb514678b6db2ac5df408e36eb39665ac355f
WangYan <WangYan@188.com>
2012-09-11 12:55:11 +0800

    精简获取PHP版本号的代码

--- deb.sh
+++ deb.sh
@@ -541,7 +541,7 @@ if [ "$PHP_VER" = "1" ]; then
 	fi
 else
 	if [ ! -s php-5.4.*.tar.gz ]; then
-		LATEST_PHP_VERSION=`curl -s http://www.php.net/downloads.php |grep "Current stable" |awk '{print $3}'`
+		LATEST_PHP_VERSION=`curl -s http://www.php.net/downloads.php | awk '/Current stable/{print $3}'`
 		LATEST_PHP_LINK="http://us.php.net/distributions/php-${LATEST_PHP_VERSION}.tar.gz"
 		BACKUP_PHP_LINK="http://wangyan.org/download/lanmp/php-latest.tar.gz"
 		Extract ${LATEST_PHP_LINK} ${BACKUP_PHP_LINK}
